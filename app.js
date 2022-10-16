723fa6796cb919d753118fd9f1075e01e70e1f01
rodriguez-miguel26 <85662329+rodriguez-miguel26@users.noreply.github.com>
2022-10-15 23:48:29 -0700

    started first 3 sections

---
+++ app.js
@@ -0,0 +1,4 @@
+const qwerty= document.getElementById('qwerty');
+const phrase= document.getElementById('phrase');
+const btnReset= document.getElementsByClassName('btn_reset');
+console.log(btnReset);
